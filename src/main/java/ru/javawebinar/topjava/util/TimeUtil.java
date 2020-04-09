d0f62b599fc6a2a0528e1e32684c3644d80a0e62
blaec <odeskonst@yandex.ru>
2020-04-09 11:01:25 +0300

    1_1_HW0_streams

--- src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
@@ -3,7 +3,7 @@ package ru.javawebinar.topjava.util;
 import java.time.LocalTime;
 
 public class TimeUtil {
-    public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
+    public static boolean isBetweenInclusive(LocalTime lt, LocalTime startTime, LocalTime endTime) {
         return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) < 0;
     }
 }
