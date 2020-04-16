60fc9dfd68c3ebb45135d5a62d33ade747ea65ce
blaec <odeskonst@yandex.ru>
2020-04-16 23:03:07 +0300

    add meals to sql scripts

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
@@ -16,10 +17,20 @@ CREATE TABLE users
 );
 CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
 
+CREATE TABLE meals
+(
+  id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  user_id          INTEGER                 NOT NULL,
+  date_time        TIMESTAMP DEFAULT now() NOT NULL,
+  description      VARCHAR                 NOT NULL,
+  calories         INTEGER                 NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
 CREATE TABLE user_roles
 (
-  user_id INTEGER NOT NULL,
-  role    VARCHAR,
+  user_id          INTEGER                 NOT NULL,
+  role             VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,10 +1,22 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
   ('User', 'user@yandex.ru', 'password'),
   ('Admin', 'admin@gmail.com', 'admin');
+
+INSERT INTO meals (user_id, date_time, description, calories) VALUES
+  (100000, '2020-01-30T10:00:00', 'Завтрак', 500),
+  (100000, '2020-01-30T13:00:00', 'Обед', 1000),
+  (100000, '2020-01-30T20:00:00', 'Ужин', 500),
+  (100000, '2020-01-31T00:00:00', 'Еда на граничное значение', 100),
+  (100000, '2020-01-31T10:00:00', 'Завтрак', 1000),
+  (100000, '2020-01-31T13:00:00', 'Обед', 500),
+  (100000, '2020-01-31T20:00:00', 'Ужин', 410),
+  (100000, '2020-02-01T14:00:00', 'Админ ланч', 510),
+  (100000, '2020-02-01T21:00:00', 'Админ ужин', 1500);
 
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
